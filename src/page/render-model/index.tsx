735e815579aa79769575a6a3566975466bb3a4f4
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-03 01:16:49 +0900

    Feat: 부품 분류 및 컴포넌트 분리

--- src/page/render-model/index.tsx
+++ src/page/render-model/index.tsx
@@ -9,15 +9,15 @@ const RenderModel = () => {
       style={{ width: "100%", height: "100%" }}
       camera={{ position: [0, 0, 55], fov: 55 }}
     >
-      <group rotation={[0.5, 0, 0]} position={[0, -5, 0]}>
+      <group rotation={[0.5, 0, 0]} position={[0, -6, 0]}>
         <MacLaptop />
       </group>
       <Environment preset="warehouse" />
       <OrbitControls
         enablePan={false}
         enableZoom={false}
-        minPolarAngle={Math.PI / 2.2}
-        maxPolarAngle={Math.PI / 2.2}
+        minPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
       />
     </Canvas>
   );
