62d4bf69d3daa612063746f15ae788698528c130
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-05 19:31:51 +0900

    Feat: Loader Suspense Progress 기능 적용

--- src/entities/rendering/coffee-shop/index.tsx
+++ src/entities/rendering/coffee-shop/index.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from "react";
-
 import { useGLTF } from "@react-three/drei";
 
 import { DreiGLTF } from "@/shared/types/gltf";
@@ -8,11 +6,11 @@ const CoffeeShop = () => {
   const { scene } = useGLTF("../models/gltf/starbucks/scene.gltf") as DreiGLTF;
 
   return (
-    <Suspense fallback={null}>
+    <>
       <group scale={3} rotation={[0.1, 4, 0.37]}>
         <primitive object={scene} />
       </group>
-    </Suspense>
+    </>
   );
 };
 
