fd7ff49f32ab88e1867c85fb700709062f4f510d
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-09 17:49:48 +0900

    Feat: Lock Screen 엔터 이벤트 추가 및 스타일 수정 / 폰트 추가

--- src/entities/components/lock-screen/index.tsx
+++ src/entities/components/lock-screen/index.tsx
@@ -2,21 +2,31 @@ import "@/shared/assets/css/lock-screen.css";
 import profile from "@/shared/assets/images/my-profile.png";
 
 import { useDay } from "@/shared/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const LockScreen = () => {
-  const { time } = useDay();
+  const { month, day, week, time } = useDay();
   const [open, setOpen] = useState(false);
 
-  const openScreen = () => setOpen(true);
+  const openKeydown = (e: KeyboardEvent) => {
+    if (e.key === "Enter" && !open) setOpen(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", openKeydown);
+    return () => document.removeEventListener("keydown", openKeydown);
+  }, []);
 
   return (
     <div className={`lock-screen-container ${open ? "open" : ""}`}>
+      <span className="lock-screen-date">{`${week}, ${day} ${month}`}</span>
       <span className="lock-screen-time">{time}</span>
       <div className="lock-screen-access-box">
         <img src={profile} className="lock-screen-profile" />
         <span className="lock-screen-name">소지우</span>
-        <button onClick={openScreen}>Open</button>
+        <button onClick={() => setOpen(true)}>
+          <span>OPEN</span>
+        </button>
       </div>
     </div>
   );
