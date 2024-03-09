f85fb72ed1e402e2428695067be49534cfc288ae
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-09 21:03:12 +0900

    Refactor: Application 드래그 로직 수정

--- src/entities/components/applications/index.tsx
+++ src/entities/components/applications/index.tsx
@@ -1,6 +1,6 @@
 import "@/shared/assets/css/applications.css";
 
-import { useEffect } from "react";
+import { DragEvent, MouseEvent } from "react";
 
 const APP_LITS = [
   { icon: "github", name: "GitHub" },
@@ -8,41 +8,25 @@ const APP_LITS = [
 ];
 
 const Applications = () => {
-  useEffect(() => {
-    const draggable = (el: HTMLDivElement) => {
-      const dragStart = () => {
-        el.classList.add("dragging");
-      };
+  const onDragStart = (e: DragEvent) => {
+    const target = e.target as HTMLDivElement;
+    target.classList.add("dragging");
+  };
 
-      const dragEnd = (e: DragEvent) => {
-        el.classList.remove("dragging");
-        const target = e.target as HTMLDivElement;
-        const top = e.clientY > 60 ? e.clientY - 40 : 30;
-        target.style.setProperty("top", `${top}px`);
-        target.style.setProperty("left", `${e.clientX - 40}px`);
-        target.style.setProperty("z-index", "9999");
-      };
+  const onDragEnd = (e: DragEvent) => {
+    const target = e.target as HTMLDivElement;
+    target.classList.remove("dragging");
+    const top = e.clientY > 60 ? e.clientY - 40 : 30;
+    target.style.setProperty("top", `${top}px`);
+    target.style.setProperty("left", `${e.clientX - 40}px`);
+    target.style.setProperty("z-index", "9999");
+  };
 
-      el.addEventListener("dragstart", dragStart);
-      el.addEventListener("dragend", dragEnd);
-
-      return () => {
-        el.removeEventListener("dragstart", dragStart);
-        el.removeEventListener("dragend", dragEnd);
-      };
-    };
-
-    const doubleClick = (el: HTMLDivElement) => {
-      el.addEventListener("dblclick", (e) => console.log(e));
-      return () => el.removeEventListener("dblclick", (e) => console.log(e));
-    };
-
-    const dragItems = document.querySelectorAll(
-      ".application"
-    ) as NodeListOf<HTMLDivElement>;
-    dragItems.forEach(draggable);
-    dragItems.forEach(doubleClick);
-  }, []);
+  const onDoubleClick = (e: MouseEvent<HTMLDivElement>) => {
+    const current = e.currentTarget;
+    const value = current.dataset["value"];
+    alert(value);
+  };
 
   return APP_LITS.map((app, i) => {
     const x = 20;
@@ -55,6 +39,10 @@ const Applications = () => {
         className="application"
         style={style}
         draggable={true}
+        data-value={app.icon}
+        onDoubleClick={onDoubleClick}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
       >
         <div className={`application-icon ${app.icon}`} />
         <span className="application-name">{app.name}</span>
