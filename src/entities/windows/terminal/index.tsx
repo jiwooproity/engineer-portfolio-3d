c5810a49b8d377e08626eccca9da1332b88d62a8
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-10 18:26:42 +0900

    Style: 터미널 디자인 및 내용 추가

--- src/entities/windows/terminal/index.tsx
+++ src/entities/windows/terminal/index.tsx
@@ -9,11 +9,27 @@ const Terminal = () => {
   return (
     <div className="terminal-wrapper">
       <div className="terminal-nav">
-        <div className="terminal-nav-btns-wrapper">
-          <button className="terminal-nav-btn close" onClick={onClose} />
-          <button className="terminal-nav-btn unfold" />
-          <button className="terminal-nav-btn full" />
+        <div className="terminal-nav-left-area">
+          <div className="terminal-nav-btns-wrapper">
+            <button className="terminal-nav-btn close" onClick={onClose} />
+            <button className="terminal-nav-btn unfold" />
+            <button className="terminal-nav-btn full" />
+          </div>
         </div>
+        <div className="terminal-nav-right-area">
+          <div className="terminal-sub-nav">
+            <span className="terminal-sub-nav-dir">~ (-zsh)</span>
+          </div>
+          <div className="terminal-sub-nav-add">+</div>
+        </div>
+      </div>
+      <div className="terminal-content">
+        <p>Release version v2.1.0</p>
+        <p>Created By So Jiwoo ( Front-End Developer )</p>
+        <p>Business: Dejay</p>
+        <p>E-mail: jiwooproity@naver.com</p>
+        <br />
+        <p>Thank for watching ..</p>
       </div>
     </div>
   );
