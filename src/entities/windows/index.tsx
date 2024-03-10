664afe4ee3be8ce1b44bcab602fa7a2cbbf5cfd4
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-10 17:44:22 +0900

    Fix: 각 앱 z-index 포커싱이 되지 않던 문제 수정

--- src/entities/windows/index.tsx
+++ src/entities/windows/index.tsx
@@ -15,18 +15,17 @@ const APPLICATION_LIST: { [key: string]: JSX.Element } = {
 const AppLayout = () => {
   const applications = useRecoilValue(windowHistory);
 
-  return Object.keys(applications).map((app) =>
-    applications[app].status ? (
-      <WindowLayout
-        key={app}
-        name={app}
-        width={applications[app].width}
-        height={applications[app].height}
-      >
-        {APPLICATION_LIST[app]}
-      </WindowLayout>
-    ) : null
-  );
+  return applications.map((app, i) => (
+    <WindowLayout
+      key={app.name}
+      name={app.name}
+      style={{ zIndex: applications.length - 1 === i ? "9999" : "1" }}
+      width={app.width}
+      height={app.height}
+    >
+      {APPLICATION_LIST[app.name]}
+    </WindowLayout>
+  ));
 };
 
 export { AppLayout };
--- src/entities/windows/terminal/index.tsx
+++ src/entities/windows/terminal/index.tsx
@@ -1,5 +1,22 @@
+import "@/shared/assets/css/windows/app-terminal.css";
+import { useWindows } from "@/shared/hooks";
+
 const Terminal = () => {
-  return <div></div>;
+  const { WINDOWS, closeApplication } = useWindows();
+
+  const onClose = () => closeApplication(WINDOWS.TERMINAL);
+
+  return (
+    <div className="terminal-wrapper">
+      <div className="terminal-nav">
+        <div className="terminal-nav-btns-wrapper">
+          <button className="terminal-nav-btn close" onClick={onClose} />
+          <button className="terminal-nav-btn unfold" />
+          <button className="terminal-nav-btn full" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Terminal;
