ceb332222e9aa9fb78be76db2e7bdcbceef056d9
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-10 17:48:16 +0900

    Feat: Portfolio Folder App 클릭 동작 수정 및 기능 추가

--- src/widgets/folder-app/ui/file-list.tsx
+++ src/widgets/folder-app/ui/file-list.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+
 const FILE_LIST = [
-  { name: "BraveGirls Fansite", img: "../images/files/bg.ico", link: "https://www.bglovely.com" },
-  { name: "SkinClouds", img: "../images/files/skinclouds.png", link: "http://www.skinclouds.net" },
+  {
+    name: "BG Fansite",
+    title: "BraveGirls Fansite",
+    img: "../images/files/bg.ico",
+    link: "https://www.bglovely.com",
+  },
+  {
+    name: "SkinClouds",
+    title: "Mincraft Skin Cloud",
+    img: "../images/files/skinclouds.png",
+    link: "http://www.skinclouds.net",
+  },
 ];
 
 const FileList = () => {
+  const [select, setSelect] = useState("");
+
   const linkToWebSite = (url: string) => {
     const link = document.createElement("a");
     link.href = url;
@@ -11,12 +25,33 @@ const FileList = () => {
     link.click();
   };
 
+  const clickHover = (name: string) => {
+    setSelect(name);
+  };
+
+  const initHover = () => {
+    setSelect("");
+  };
+
+  useEffect(() => {
+    const container = document.querySelector(".file-list-wrapper") as HTMLDivElement;
+
+    container?.addEventListener("click", initHover);
+    return () => {
+      container?.removeEventListener("click", initHover);
+    };
+  }, []);
+
   return (
     <div className="file-list-wrapper">
-      {FILE_LIST.map(({ name, img, link }) => (
-        <div className="file-list-item" onClick={() => linkToWebSite(link)}>
-          <img src={img} title={name} />
-          <span>{name}</span>
+      {FILE_LIST.map(({ name, title, img, link }) => (
+        <div
+          className="file-list-item"
+          onClick={() => clickHover(name)}
+          onDoubleClick={() => linkToWebSite(link)}
+        >
+          <img className={name === select ? "active" : ""} src={img} title={title} />
+          <span className={name === select ? "active" : ""}>{name}</span>
         </div>
       ))}
     </div>
